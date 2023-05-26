bac9c3281b851916bb2ffb976573f4d4421cd0fb
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-05-26 04:09:14 +0300

    Added wrapping for platform icons list

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,7 @@ import { MdPhoneIphone } from 'react-icons/md';
 import { SiNintendo } from 'react-icons/si';
 import { BsGlobe } from 'react-icons/bs';
 import { IconType } from 'react-icons/lib/cjs';
-import { HStack, Icon } from '@chakra-ui/react';
+import { HStack, Icon, Wrap, WrapItem } from '@chakra-ui/react';
 import { Platform } from '../hooks/useGames';
 
 interface Props {
@@ -32,9 +32,17 @@ const PlatformIconList = ({ platforms }: Props) => {
 
 	return (
 		<HStack marginY="10px">
-			{platforms.map(platform => (
-				<Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
-			))}
+			<Wrap>
+				{platforms.map(platform => (
+					<WrapItem>
+						<Icon
+							key={platform.id}
+							as={iconMap[platform.slug]}
+							color="gray.500"
+						/>
+					</WrapItem>
+				))}
+			</Wrap>
 		</HStack>
 	);
 };
