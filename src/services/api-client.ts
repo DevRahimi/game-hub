41e7065bb4359e140ada8144a34def5b7449c670
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2024-03-26 21:37:44 +0200

    Add .env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+
 export default axios.create({
 	baseURL: 'https://api.rawg.io/api',
 	params: {
-		key: '65a4c9e5a4664f439b5d3b609e217d4a',
+		key: apiKey,
 	},
 });
