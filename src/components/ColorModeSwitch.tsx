b99cb66e21d6f6a8372bc403a06fdd95a40d7370
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2024-03-26 21:55:55 +0200

    Convert from slider to icon button

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,12 +1,18 @@
-import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
+import { HStack, IconButton, useColorMode } from '@chakra-ui/react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 const ColorModeSwitch = () => {
 	const { toggleColorMode, colorMode } = useColorMode();
 
 	return (
 		<HStack>
-			<Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-			<Text whiteSpace='nowrap'>Dark Mode</Text>
+			{/* <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} /> */}
+			<IconButton
+				borderRadius='50%'
+				aria-label={colorMode === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'}
+				icon={colorMode === 'dark' ? <FaSun /> : <FaMoon />}
+				onClick={toggleColorMode}
+			/>
 		</HStack>
 	);
 };
