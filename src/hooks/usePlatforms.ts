acf98bdf5e8533f8cb5754847bcb5ad60b50ec21
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2024-04-28 14:57:32 +0200

    Refactor: remove duplicate interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,18 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import apiClient, { FetchResponse } from "../services/api-client";
 
-interface Platform {
-	id: number;
-	name: string;
-	slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 const usePlatforms = () =>
-	useQuery({
-		staleTime: 24 * 60 * 60 * 1000, // 24 hours
-		queryKey: ["platforms"],
-		queryFn: () => apiClient.get<FetchResponse<Platform>>("/platforms/lists/parents").then((res) => res.data),
-		initialData: { count: platforms.length, results: platforms },
-	});
+  useQuery({
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Platform>>("/platforms/lists/parents").then((res) => res.data),
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
