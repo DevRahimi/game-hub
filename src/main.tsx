2acdce4e8142cf37fefe2a7327ff40063ce8cefa
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2024-04-28 14:38:42 +0200

    Formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 import theme from "./theme";
