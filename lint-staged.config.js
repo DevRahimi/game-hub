19b3d29ac0a39772fdd254cb5919af695cbf13b8
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2025-06-11 02:25:03 +0300

    add lint and build github workflow, update husky hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "*.{js,jsx,ts,tsx}": ["bun run format", "bun run lint:fix"],
+  "*.{js,jsx,ts,tsx}": ["bun run format", "bun run lint"],
 };
